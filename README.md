c0d7219e6a7dc7bc258c5c8542b475348934205a
Souvik-R <roysouvikr112@gmail.com>
2024-11-18 01:52:43 +0530

    setup redux store

--- README.md
+++ README.md
@@ -10,3 +10,12 @@
 - Create BrowserRouter => Routes => Route=/ Body > RouteChildren
 - Create an Outlet in your body component
 - Create a Footer
+- Install axios
+- CORS - install cors in backend => add middleware with configurations: origin, credentials: true
+- Whenever you are making an API call so pass axios => { withCredentials: true }
+- npm install @reduxjs/toolkit react-redux - https://redux-toolkit.js.org/tutorials/quick-start
+- configureStore => Provider => createSlice => add reducer to store.
+- Add redux devtools in chrome
+- Login and see if your data is coming properly in the store
+- Navbar should update as soon as users logs in 
+- Refactor our code to add constants file + create a components folder
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Body from "./Body";
-import Login from "./Login";
-import Profile from "./Profile";
-import Footer from "./Footer";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Footer from "./components/Footer";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./components/Feed";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/footer" element={<Footer />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/footer" element={<Footer />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
