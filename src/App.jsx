7f37a0c5b8f360a86799394776a3d5e7a37566e5
Souvik-R <roysouvikr112@gmail.com>
2025-02-01 01:56:12 +0530

    feat: connection list

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import Feed from "./components/Feed";
+import Connections from "./components/Connections";
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
               <Route path="/" element={<Feed />} />
               <Route path="/login" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
-              <Route path="/footer" element={<Footer />} />
+              {/* <Route path="/footer" element={<Footer />} /> */}
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/request" element={<Footer />} />
             </Route>
           </Routes>
         </BrowserRouter>
